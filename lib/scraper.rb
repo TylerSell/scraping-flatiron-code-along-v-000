824dd77578b75c4577dd4009ddd8df7e480f0ed5
tylerselldesign <tylerselldesign@gmail.com>
2018-09-27 22:45:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -18,6 +19,7 @@ class Scraper
   
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
   
   def get_courses 
@@ -29,6 +31,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page
 
 
 
